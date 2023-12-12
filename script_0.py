7830427a2665be24a8bde66f874d9ace7012a488
Walusimbi Isiah.com <isiahwalusimbi@gmail.com>
2023-12-12 17:02:42 +0300

    scrip_0

--- script_0.py
+++ script_0.py
@@ -1,17 +1,26 @@
+'''importing moduels'''
 from bs4 import BeautifulSoup
-import requests 
+import requests
+from urllib.error import HTTPError
+from urllib.error import URLError 
+
+'''function for the scraper'''
 def data(url):
+	try:
+		response = requests.get(url)
+	except HTTPError as e:
+	 	print(e)
+	except URLError as e:
+	 	print('The server could not be found!')
+	else:
+		data = response.text
+		soup = BeautifulSoup(data,'html.parser')
+		items = soup.find_all('div',{'class':"listing__content__wrapper"})
+		for item in items:
+		    contact = item.find('h4').text
+		    cname =item.find('a',{'class':"listing__name--link listing__link jsListingName"}).text[2:]
+		    print('Company Name:',cname)
+		    print('Çontact:',contact)
 
-	response = requests.get(url)
-
-	print(response)
-
-	data = response.text
-	soup = BeautifulSoup(data,'html.parser')
-	items = soup.find_all('div',{'class':"listing__content__wrapper"})
-	for item in items:
-	    contact = item.find('h4').text
-	    cname =item.find('a',{'class':"listing__name--link listing__link jsListingName"}).text[2:]
-	    print('Company Name:',cname)
-	    print('Çontact:',contact)
-data("https://www.yellowpages.ca/search/si/1/health+wellness/Toronto+ON") 
+'''function calling'''
+data("https://www.yellowpages.ca/search/si/1/health+wellness/Toronto+ON")
